b33a1461789f4c485613a6ade437a202e198dec7
amirhosein sajjadi <canadavancouver2003@gmail.com>
2025-01-21 22:48:19 +0330

    finish

--- database/migrations/2025_01_20_170854_create_requests_table.php
+++ database/migrations/2025_01_20_170854_create_requests_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->unsignedBigInteger("company_id");
             $table->foreign("company_id")->references("id")->on("companies")->onDelete("cascade");
 
+            $table->string("skill");
+
             $table->unsignedBigInteger("user_id");
             $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
 
